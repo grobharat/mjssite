ae03760dfe8a319686a84d8afe7e88e2b8de0891
Grobharat <gitgrobharat@gmail.com>
2024-07-12 12:10:07 +0530

    Auth start

---
+++ resources/views/auth/pages/login.blade.php
@@ -0,0 +1,31 @@
+@extends('auth.layouts.default')
+@section('customhead')
+<title>{{ env('APP_NAME') }} | Login</title>
+    <!-- Custom styles for this template -->
+    <link href="bootstrap-4.1.3-dist/customcss/signin.css" rel="stylesheet">
+@endsection
+
+@section('content')
+<form class="form-signin">
+      <img class="mb-4" src="{{url('logo.jpg')}}" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2010-<?php echo date("Y"); ?>
+      </p>
+    </form>
+@endsection
+
+
+@section('customfooter')
+
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Modules\auth\Controllers\AuthController as auth;
+//------------Website Module--------//
 Route::get('/', function () {
     return view('website/pages/home');
 });
@@ -14,3 +16,14 @@ Route::get('/products', function () {
 Route::get('/portfolio', function () {
     return view('website/pages/portfolio');
 });
+
+//------------auth Module--------//
+//Open Routes
+Route::get('register', [auth::class,'registerview']);
+Route::post('register', [auth::class,'register']);
+Route::get('login', [auth::class,'loginview']);
+Route::post('login', [auth::class,'login']);
+Route::get('forgotpassword', [auth::class,'forgotpasswordview']);
+Route::post('forgotpassword', [auth::class,'forgotpassword']);
+Route::get('resetpassword', [auth::class,'resetpasswordview']);
+Route::post('resetpassword', [auth::class,'resetpassword']);
